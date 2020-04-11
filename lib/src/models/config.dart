6778da3bd4c13f3c44565b6a8656a75604db21e2
Dmitry Krutskikh <dmitry.krutskikh@gmail.com>
2020-04-11 13:28:34 +0300

    add linesOfCodeDefaultWarningLevel / linesOfCodeInComponentWarningLevel and default values to config

--- lib/src/models/config.dart
+++ lib/src/models/config.dart
@@ -2,6 +2,8 @@ import 'package:meta/meta.dart';
 
 const cyclomaticComplexityDefaultWarningLevel = 20;
 const executableLinesOfCodeDefaultWarningLevel = 50;
+const linesOfCodeDefaultWarningLevel = 100;
+const linesOfCodeInComponentDefaultWarningLevel = 1000;
 const numberOfArgumentsDefaultWarningLevel = 4;
 
 /// Reporter config to use with various [Reporter]s
@@ -9,13 +11,18 @@ const numberOfArgumentsDefaultWarningLevel = 4;
 class Config {
   final int cyclomaticComplexityWarningLevel;
   final int executableLinesOfCodeWarningLevel;
+  final int linesOfCodeWarningLevel;
+  final int linesOfCodeInComponentWarningLevel;
   final int numberOfArgumentsWarningLevel;
 
-  const Config(
-      {this.cyclomaticComplexityWarningLevel =
-          cyclomaticComplexityDefaultWarningLevel,
-      this.executableLinesOfCodeWarningLevel =
-          executableLinesOfCodeDefaultWarningLevel,
-      this.numberOfArgumentsWarningLevel =
-          numberOfArgumentsDefaultWarningLevel});
+  const Config({
+    this.cyclomaticComplexityWarningLevel =
+        cyclomaticComplexityDefaultWarningLevel,
+    this.executableLinesOfCodeWarningLevel =
+        executableLinesOfCodeDefaultWarningLevel,
+    this.linesOfCodeWarningLevel = linesOfCodeDefaultWarningLevel,
+    this.linesOfCodeInComponentWarningLevel =
+        linesOfCodeInComponentDefaultWarningLevel,
+    this.numberOfArgumentsWarningLevel = numberOfArgumentsDefaultWarningLevel,
+  });
 }
